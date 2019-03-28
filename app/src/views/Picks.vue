c0409323b7493b8df5bb23ba4821ff400faa807a
Donnie Mattingly <donnie@Donnies-MacBook-Pro.local>
2019-03-27 21:12:20 -0400

    fix gitignore

--- app/src/views/Picks.vue
+++ app/src/views/Picks.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PicksComponent msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import PicksComponent from '@/components/PicksComponent.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    PicksComponent,
   },
 };
 </script>
