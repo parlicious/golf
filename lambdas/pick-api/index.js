348b07ea62bf4984bd712a52b89e924cc2e0cc19
Donnie Mattingly <donniewmattingly@gmail.com>
2019-03-28 17:16:03 -0400

    add simple listing of picks bucket

--- lambdas/pick-api/index.js
+++ lambdas/pick-api/index.js
@@ -1,37 +1,57 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-/**
 
- */
-exports.handler = (event, context, callback) => {
+
+exports.handler = async (event, context, callback) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
-    const done = (err, res) => callback(null, {
-        statusCode: err ? '400' : '200',
-        body: err ? err.message : JSON.stringify(res),
-        headers: {
-            'Content-Type': 'application/json',
-        },
+    const body = JSON.parse(event.body);
+    const tournament = body['tournament'];
+    const year = body['year'];
+    const prefix = `picks/${tournament}/${year}/individual`;
+
+    const searchParams = {
+        Bucket: 'parlicious-data',
+        Prefix: prefix
+    };
+
+
+    let data;
+    try {
+        data = await s3.listObjectsV2(searchParams).promise();
+    } catch (e) {
+        console.log(e);
+    }
+
+    data.Contents.forEach((o) => {
+        console.log(o);
     });
 
-    switch (event.httpMethod) {
-        case 'DELETE':
-            console.log('Delete');
-            done(false, JSON.parse(event.body));
-            break;
-        case 'GET':
-            console.log('Get');
-            done(false, JSON.parse(event.body));
-            break;
-        case 'POST':
-            console.log('Post');
-            done(false, JSON.parse(event.body));
-            break;
-        case 'PUT':
-            console.log('Put');
-            done(false, JSON.parse(event.body));
-            break;
-        default:
-            done(new Error(`Unsupported method "${event.httpMethod}"`));
+    return {
+        statusCode: '200',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        }
     }
+    // switch (event.httpMethod) {
+    //     case 'DELETE':
+    //         console.log('Delete');
+    //         done(false, JSON.parse(event.body));
+    //         break;
+    //     case 'GET':
+    //         console.log('Get');
+    //         done(false, JSON.parse(event.body));
+    //         break;
+    //     case 'POST':
+    //         console.log('Post');
+    //         done(false, JSON.parse(event.body));
+    //         break;
+    //     case 'PUT':
+    //         console.log('Put');
+    //         done(false, JSON.parse(event.body));
+    //         break;
+    //     default:
+    //         done(new Error(`Unsupported method "${event.httpMethod}"`));
+    // }
 };
