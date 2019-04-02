8933f0bfc3885ef67d9a666d20a5d06f5d2a4bb7
Brice Rising <rising.brice@gmail.com>
2019-04-02 18:39:49 -0400

    Write script that builds webdsite and lambdas
    This script will build the website and lambdas and also create a tfvars file with information necessary to deploy the site + backend services

---
+++ cicd/scripts/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+LIFECYCLE=${1}
+DATA_BUCKET_NAME="parlicious-data-${LIFECYCLE}"
+
+if [ -z ${LIFECYCLE} ] ; then
+    echo "You must enter a lifecycle. CAUTION: uat AND prod ARE RESERVED BY THE PIPELINE"
+    echo "USAGE: build.sh <LIFECYCLE>"
+    exit 2
+fi
+
+LAMBDA_FOLDERS=$(ls lambdas | awk ' BEGIN { ORS = ""; print "["; } { print "\/\@"$0"\/\@"; } END { print "]"; }' | sed "s^\"^\\\\\"^g;s^\/\@\/\@^\", \"^g;s^\/\@^\"^g")
+
+cd web
+
+npm install
+npm run build
+
+cd - > /dev/null
+
+cat > terraform/parlicious/${LIFECYCLE}.tfvars << EOF
+lambda_folders = ${LAMBDA_FOLDERS}
+data_bucket_name = "${DATA_BUCKET_NAME}"
+EOF
