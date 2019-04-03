d0974bcde2e580d8468451eb4732357e1b429cb1
Brice Rising <rising.brice@gmail.com>
2019-04-02 21:10:32 -0400

    Write terraform for parlicious deployments
    This terraform will deploy parlicious for different lifecycles after builds are completed

---
+++ terraform/parlicious/iam.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "parlicious_lambdas_role" {
+  name = "parlicious-lambdas-role-${var.lifecycle}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "test_policy" {
+  name = "parlicious-lambdas-role-policy"
+  role = "${aws_iam_role.parlicious_lambdas_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:logs:*:*:*"
+    }
+  ]
+}
+EOF
+}
