dbdea0765c75616aa498de83409cf193cc3e4a17
Donnie Mattingly <donnie@Donnies-MacBook-Pro.local>
2019-04-04 10:15:52 -0400

    load test data

--- web/src/components/PicksComponent.vue
+++ web/src/components/PicksComponent.vue
@@ -57,6 +57,7 @@ export default {
     async getIndividualPicks() {
       this.loading = true;
       const picks = PicksService.getIndividualPicks(this.email);
+      this.selectEmail = false;
       this.loading = false;
     },
   },
