a9b73d45e4916d3fb877eaf4758cf16dc5ed8e76
Brice Rising <rising.brice@gmail.com>
2019-04-03 20:13:07 -0400

    Make dev and prod pipelines
    The dev pipeline needs to implement verification checks for PRs, and the prod pipeline will be a manual pipeline that deploys prod

--- terraform/codebuild/codebuild.tf
+++ terraform/codebuild/codebuild.tf
@@ -38,7 +38,82 @@ resource "aws_codebuild_project" "parlicious_uat" {
   }
 }
 
-resource "aws_codebuild_webhook" "parlicious" {
+resource "aws_codebuild_project" "parlicious_dev" {
+  name          = "parlicious-dev"
+  description   = "parlicious dev pipeline"
+  build_timeout = "10"
+  service_role  = "${aws_iam_role.parlicious_cicd_role.arn}"
+  badge_enabled = true
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type     = "S3"
+    location = "${aws_s3_bucket.parlicious_cicd_cache.bucket}"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "${aws_ecr_repository.parlicious_cicd_agent.repository_url}"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/parlicious/golf.git"
+    git_clone_depth = 1
+    buildspec       = "cicd/buildspecs/dev.buildspec.yaml"
+
+    auth = {
+      type     = "OAUTH"
+    }
+  }
+}
+
+resource "aws_codebuild_project" "parlicious_prod" {
+  name          = "parlicious-prod"
+  description   = "parlicious prod pipeline"
+  build_timeout = "10"
+  service_role  = "${aws_iam_role.parlicious_cicd_role.arn}"
+  badge_enabled = true
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  cache {
+    type     = "S3"
+    location = "${aws_s3_bucket.parlicious_cicd_cache.bucket}"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "${aws_ecr_repository.parlicious_cicd_agent.repository_url}"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/parlicious/golf.git"
+    git_clone_depth = 1
+    buildspec       = "cicd/buildspecs/prod.buildspec.yaml"
+
+    auth = {
+      type     = "OAUTH"
+    }
+  }
+}
+
+resource "aws_codebuild_webhook" "parlicious_uat" {
   project_name  = "${aws_codebuild_project.parlicious_uat.name}"
   branch_filter = "master"
+}
+
+resource "aws_codebuild_webhook" "parlicious_dev" {
+  project_name  = "${aws_codebuild_project.parlicious_dev.name}"
+  branch_filter = ":^(?!(<master>)).*"
 }
