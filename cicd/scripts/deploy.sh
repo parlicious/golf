ddbfb6d72ba02777c92cff27d00d8ff338dc46a0
Unknown <tfcorcor@gmail.com>
2019-04-06 10:12:49 -0400

    more debugging

--- cicd/scripts/deploy.sh
+++ cicd/scripts/deploy.sh
@@ -30,6 +30,9 @@ aws s3 sync web/dist s3://${WEBSITE_BUCKET_NAME}
 echo "** UI Bucket Sync Done **"
 
 TOTAL_OBJECTS=$(aws s3 ls s3://${DATA_BUCKET_NAME} --summarize  | grep "Total Objects: 0")
+
+echo $TOTAL_OBJECTS
+
 if [ "Total Objects: 0" == "${TOTAL_OBJECTS}" ] ; then
     aws s3 sync datatemplates s3://${DATA_BUCKET_NAME}
 fi
