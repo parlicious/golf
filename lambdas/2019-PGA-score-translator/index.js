949d084e6adef79f5b1daa21490244c0692c7e93
Donnie Mattingly <donniewmattingly@gmail.com>
2019-04-16 20:25:13 -0400

    add config to pga leaderboard generator

--- lambdas/2019-PGA-score-translator/index.js
+++ lambdas/2019-PGA-score-translator/index.js
@@ -1,104 +1,108 @@
 // testing s3 deploy test
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+const AWS = require('aws-sdk');
 
-var https = require('https');
+const s3 = new AWS.S3();
 
-var host = 'data.pga.com';
-var path = '/event/R014/2019/leaderboard.json';
+const https = require('https');
 
-var options = {
-    host: host,
-    path: path,
-    method: 'GET'
+const host = 'data.pga.com';
+const path = '/event/R014/2019/leaderboard.json';
+
+const options = {
+  host,
+  path,
+  method: 'GET',
 };
 
-exports.handler = (event, context, callback) => {
-    buildleaderboard(callback, 1);
-};
+function buildleaderboard(callback, count) {
+  const newcount = count + 1;
+  console.log(`count: ${count}`);
+  const leaderboard = {
+    version: 1,
+    round: null,
+    cut_line: null,
+    cut_penalty: null,
+    timezone: 'EDT',
+    refreshed: Date.now(),
+    players: [
 
-function buildleaderboard(callback, count){
-    const newcount = count + 1;
-    console.log(`count: ${count}`);
-    let leaderboard = {
-        "version": 1,
-        "round": null,
-        "cut_line": null,
-        "cut_penalty": null,
-        "timezone": "EDT",
-        "refreshed": Date.now(),
-        "players": [
-    
-        ]
-    };
-    let req = https.request(options, (res) => {
-        let data = '';
-        res.on('data', function (chunk) {
-            data += chunk;
-        });
-
-        res.on('error', function (e) {
-            console.log(e);
-        });
-
-        res.on('end', () => {
-            let pgadata = JSON.parse(data);
-
-            //build our model
-            leaderboard.cut_line = pgadata.roundTotal == 2 ? pgadata.tournamentCut.projectedCut : null;
-            leaderboard.round = pgadata.roundTotal;
-            let players = pgadata.player;
-            for(var i = 0; i < players.length; i++) {
-                let player = players[i];
-                let newplayer = {
-                    'id': parseInt(player.id),
-                    'first_name': player.firstName,
-                    'last_name': player.lastName,
-                    'thru': player.thru,
-                    'teetime': player.currentTeetime,
-                    'today': player.currentParRelative,
-                    'to_par': player.totalParRelative == "-" ? getScoreTotal(player.round) : player.totalParRelative,
-                    'position': player.currentPosition,
-                    "individual_pen": player.currentPosition == "CUT" ? leaderboard.cut_penalty * 2 : null,
-                    "individual_bonus": ((leaderboard.round == 3 || leaderboard.round == 4) && player.currentPosition == "1") ? leaderboard.cut_penalty * -2 : null,
-                    "status": player.currentPosition == "CUT" ? "C" : "A"
-                };
-                leaderboard.players.push(newplayer);
-            }
-            const key = "leaderboards/pga/2019/leaderboard.json";
-        
-            let params = {
-                Bucket : process.env.LEADERBOARD_BUCKET,
-                Key : key,
-                Body : JSON.stringify(leaderboard),
-                ACL:'public-read',
-                ContentType: "application/json"
-            };
-            s3.putObject(params, function(err, data) {
-                if (err) 
-                {
-                    console.log(err, err.stack); // an error occurred
-                }
-                else {
-                    console.log(data);           // successful response
-                    if(count < 5) {
-                        setTimeout(() => buildleaderboard(callback, newcount), 10000);
-                    } else callback(null,leaderboard);
-                 }
-            });
-        });
+    ],
+  };
+  const req = https.request(options, (res) => {
+    let data = '';
+    res.on('data', (chunk) => {
+      data += chunk;
     });
-    req.end();
+
+    res.on('error', (e) => {
+      console.log(e);
+    });
+
+    res.on('end', () => {
+      const pgadata = JSON.parse(data);
+
+      // build our model
+      leaderboard.cut_line = pgadata.roundTotal === 2 ? pgadata.tournamentCut.projectedCut : null;
+      leaderboard.round = pgadata.roundTotal;
+      const players = pgadata.player;
+      for (let i = 0; i < players.length; i++) {
+        const player = players[i];
+        const newplayer = {
+          id: parseInt(player.id),
+          first_name: player.firstName,
+          last_name: player.lastName,
+          thru: player.thru,
+          teetime: player.currentTeetime,
+          today: player.currentParRelative,
+          to_par: player.totalParRelative === '-' ? getScoreTotal(player.round) : player.totalParRelative,
+          position: player.currentPosition,
+          individual_pen: player.currentPosition === 'CUT' ? leaderboard.cut_penalty * 2 : null,
+          individual_bonus: ((leaderboard.round === 3 || leaderboard.round === 4) && player.currentPosition === '1') ? leaderboard.cut_penalty * -2 : null,
+          status: player.currentPosition === 'CUT' ? 'C' : 'A',
+        };
+        leaderboard.players.push(newplayer);
+      }
+      const key = 'leaderboards/pga/2019/leaderboard.json';
+
+      const params = {
+        Bucket: process.env.LEADERBOARD_BUCKET,
+        Key: key,
+        Body: JSON.stringify(leaderboard),
+        ACL: 'public-read',
+        ContentType: 'application/json',
+      };
+      s3.putObject(params, (err, data) => {
+        if (err) {
+          console.log(err, err.stack); // an error occurred
+        } else {
+          console.log(data); // successful response
+          if (count < 5) {
+            setTimeout(() => buildleaderboard(callback, newcount), 10000);
+          } else callback(null, leaderboard);
+        }
+      });
+    });
+  });
+  req.end();
 }
 
 function getScoreTotal(rounds) {
-    let total = 0;
-    
-    for(let i = 0; i < rounds.length; i++) {
-        let round = rounds[i];
-        let score = parseInt(round.parRelative, 10);
-        if(!isNaN(score)) total = total + score;
-    }
+  let total = 0;
 
-    return total;
+  for (let i = 0; i < rounds.length; i++) {
+    const round = rounds[i];
+    const score = parseInt(round.parRelative, 10);
+    if (!isNaN(score)) total += score;
+  }
+
+  return total;
 }
+
+exports.handler = (event, context, callback) => {
+  buildleaderboard(callback, 1);
+};
+
+// uncomment for local testing with: node index.js
+// exports.handler(null, null, (res) => {
+//   console.log(res);
+// });
