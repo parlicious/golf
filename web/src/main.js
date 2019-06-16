bb23cb87459f0b5cf62906f28e8104e0f8c68c2f
Donnie Mattingly <you@example.com>
2019-06-16 19:14:22 -0400

    initial pwa commit

--- web/src/main.js
+++ web/src/main.js
@@ -18,3 +18,12 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app');
+
+const prod = process.env.NODE_ENV === 'production';
+// const shouldSW = 'serviceWorker' in navigator && prod;
+const shouldSW = true;
+if (shouldSW) {
+  navigator.serviceWorker.register('/service-worker.js').then(() => {
+    console.log('Service Worker Registered!');
+  });
+}
